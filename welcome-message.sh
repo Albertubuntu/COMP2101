778d0c4fed1d127accefe4a70096444bb4cfdad0
Albertubuntu <60078982+Albertubuntu@users.noreply.github.com>
2020-02-04 20:00:31 -0500

    Add files via upload

--- welcome-message.sh
+++ welcome-message.sh
@@ -1,59 +1,30 @@
 #!/bin/bash
 #
-# This script produces a dynamic welcome message
+# This script produces the dynamic welcome message
 # it should look like
 #   Welcome to planet hostname, title name!
 
-# Task 1: Use the variable $USER instead of the myname variable to get your name
-# Task 2: Dynamically generate the value for your hostname variable using the hostname command - e.g. $(hostname)
-# Task 3: Add the time and day of the week to the welcome message using the format shown below
+# Improve this script by using the value in the automatic variable $USER for the name
+# Improve this script by adding some time and day of the week information to the welcome message
 #   Use a format like this:
-#   It is weekday at HH:MM AM.
-# Task 4: Set the title using the day of the week
-#   e.g. On Monday it might be Optimist, Tuesday might be Realist, Wednesday might be Pessimist, etc.
-#   You will need multiple tests to set a title
-#   Invent your own titles, do not use the ones from this example
+#   It is HH:MM AM on weekday.
 
 ###############
-# Variables #
+# Variables   #
 ###############
 name="$USER"
 date=$(date +'%I:%M %p')
 hostname=$(hostname)
-day=$(date +%A)
+weekday=$(date +%u)
 ###############
-# Main #
+# Main        #
 ###############
 cat <<EOF
-if [ "$day" = "Monday" ]
-then
-Welcome to Earth $hostname, Optimist $name!
-else
-if [ "$day" = "Tuesday" ]
-then
-Welcome to Earth $hostname, Realist $name!
-else
-if [ "$day" = "Wednesday" ]
-then
-Welcome to Earth $hostname, Pessimist $name!
-else
-if [ "$day" = "Thursday" ]
-then
-Welcome to Earth $hostname, kathi $name!
-else
-if [ "$day" = "Friday" ]
-then
-Welcome to Earth $hostname, personalise $name!
-else
-if [ "$day" = "Saturday" ]
-then
-Welcome to Earth $hostname, Pessionist $name!
-else
-Welcome to Earth $hostname, motivated $name!
-fi
-fi
-fi
-fi
-fi
+Welcome to Earth $hostname, $name!
 EOF
-echo "It is $date on $day ."
+if [ "$weekday" = "6" ] || [ "$weekday" = "7" ]
+then
+   echo "It is $date on Weekend."
+else
+   echo "It is $date on Weekday."
+fi
